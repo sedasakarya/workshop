a70f831e4226ae612220db7e6b8848d183c883ba
Seda Sakarya <sedasakarya77@gmail.com>
2020-05-18 15:55:06 +0200

    Workshop commit

--- PYTHON/3.Data-Types/data_types.py
+++ PYTHON/3.Data-Types/data_types.py
@@ -48,7 +48,7 @@ Three
 s = "python"
 ss = "i have two cats YEAH!"
 
-print( len(s) ) # len is a function here. It gives us the length.
+# print( len(s) ) # len is a function here. It gives us the length.
 
 # print( s.capitalize() ) # Capitalize is a method and s is an object.
 # print( s.upper() ) # If you don't put parenthesis after the method, it is like a variable. BEWARE !!! It looks red.
@@ -58,14 +58,14 @@ print( len(s) ) # len is a function here. It gives us the length.
 # REPLACE PARTS OF THE STRING
 #---------------------------------------
 #ss = ss.replace("cats", "dogs") # We change the variable here but below we just made an overview not change it.
-print( ss.replace("cats", "dogs") ) # It replaces cats with dogs.
-print(ss)
+# print( ss.replace("cats", "dogs") ) # It replaces cats with dogs.
+# print(ss)
 
 # SPLIT A STRING
 #----------------------------------------------
-print( ss.split() ) # It uses the original string. 
-print( ss.split('a') ) # It removes all the a's but not uppercase ones.
-print( ss.split('a', 1) ) # Only one a it removes.
+# print( ss.split() ) # It uses the original string. 
+# print( ss.split('a') ) # It removes all the a's but not uppercase ones.
+# print( ss.split('a', 1) ) # Only one a it removes.
 
 # CONCATENATION
 #----------------------------------------------------
@@ -78,7 +78,7 @@ age = 27
 #phrase = name + " is 27 years old" # It works
 #phrase = name + " is " + age + " years old"
 #phrase = name + " is " + str(age) + " years old" # It works
-phrase = f"{name} is {age} years old" # This is the BEST WAY !!! F is format.
+# phrase = f"{name} is {age} years old" # This is the BEST WAY !!! F is format.
 #phrase = "{} is {} years old. {}?".format(name, age, age) # It works too.
 #print(phrase)
 
@@ -95,14 +95,27 @@ b_null  = None
 # COLLECTIONS/SEQUENCES (PYTHON ARRAYS)
 #============================================
 
-# LISTS
+# LISTS (MUTABLE)
 l = [1,2,3,4]
-print( l[2] )
+# print( l[2] )
 
-# DICTIONARY
+# DICTIONARY (MUTABLE)
 d = {
   "first_name": "Marcel",
   "last_name" : "Duchamp",
   "age"       : 27
 }
-print(d["first_name"]) # Everything is like an ARRAY here.
+d["first_name"] = "Bob" # How we change the value. These are mutable values.
+# print(d["first_name"]) # Everything is like an ARRAY here.
+# We don't have index base print here. Key-based collection
+
+# TUPLES (IMMUTABLE LIKE CONST)
+t = ("january", "february", "mars")
+# print(t[1])
+
+# SET 
+# Sets are not index-based !!!
+s1 = {"apple", "car", 56} # The order of the list changes everytime we print it.
+s2 = {1,8,9,2,48,48,0,99,1} # Numbers are always in the same order. They don't change. We cant duplicate the same numbers here. It writes just once.
+# print(s1) 
+# print(s2)
---
+++ PYTHON/4.Functions/functions.py
@@ -0,0 +1,22 @@
+""" BUILD-IN FUNTIONS
+==================================================== """
+# Native functions like print...
+print("whatever")
+print("whatever", "and", "ever")
+
+age = 12
+print("age is:", age)
+#print("list\n", [1,2,3])
+#print("list", [1,2,3], sep=":", end=".") # Sep adds a seperation sign. End adds a sign at the end.
+
+# MAX, MIN AND ROUND
+#------------------------------------
+
+# MAX ¬ MIN
+l = [3,6,78,12]
+print("max value is:", max (l))
+print("min value is:", min (l))
+
+# ROUND
+num = 3.2455
+print("rounded number:", round(num, 2)) # If you write num, 2 . It tells us how many decimal it will show.
